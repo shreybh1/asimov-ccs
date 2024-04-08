3c6c95aae7987fac96d487cd8ec8c390bcdb5db5
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2024-04-08 21:03:34 +0100

    Implement a generic parallel data reorder

--- src/linear_solvers/petsc/vec_petsc.f90
+++ src/linear_solvers/petsc/vec_petsc.f90
@@ -585,8 +585,7 @@ contains
 
   end subroutine
 
-  !> PETSc implementation to get vector data in natural ordering
-  module subroutine get_natural_data_vec(par_env, mesh, v, data)
+  module subroutine reorder_data_vec(par_env, data_from, idx_to, data_to)
 
     use petsc, only: PETSC_DECIDE
     use petscvec, only: tVec, &
@@ -594,58 +593,43 @@ contains
                         VecSetValues, VecAssemblyBegin, VecAssemblyEnd, &
                         VecGetArrayReadF90, VecRestoreArrayReadF90, &
                         VecDestroy
-
+    
     class(parallel_environment), intent(in) :: par_env
-    type(ccs_mesh), intent(in) :: mesh
-    class(ccs_vector), intent(inout) :: v
-    real(ccs_real), dimension(:), allocatable, intent(out) :: data !< The returned vector data in
-    !< natural ordering. Note the use
-    !< of allocatable + intent(out),
-    !< this ensures it will be
-    !< de/reallocated by this subroutine.
+    real(ccs_real), dimension(:), intent(in) :: data_from
+    integer(ccs_int), dimension(:), intent(in) :: idx_to
+    real(ccs_real), dimension(:), intent(out) :: data_to
 
-    real(ccs_real), dimension(:), pointer :: vec_data ! The data stored in the vector
+    integer(ccs_int) :: nlocal_out, nlocal_in
 
+    real(ccs_real), dimension(:), pointer :: data_tmp
     type(tVec) :: vec_tmp
-
     integer(ccs_err) :: ierr
 
-    associate (topo => mesh%topo, &
-               local_num_cells => mesh%topo%local_num_cells)
+    nlocal_out = size(data_to)
+    nlocal_in = size(data_from)
 
-      ! Create temporary vector to hold the shuffled data
-      select type (par_env)
-      type is (parallel_environment_mpi)
-        call VecCreate(par_env%comm, vec_tmp, ierr)
-      class default
-        call error_abort("Only MPI parallel environments currently supported!")
-      end select
-      call VecSetSizes(vec_tmp, local_num_cells, PETSC_DECIDE, ierr)
-      call VecSetFromOptions(vec_tmp, ierr)
+    ! XXX: This would be better with VecCreateMPIWithArray, however the Fortran interface is
+    !      missing...
+    select type(par_env)
+    type is (parallel_environment_mpi)
+      call VecCreate(par_env%comm, vec_tmp, ierr)
+    class default
+      call error_abort("Only MPI parallel environments currently supported")
+    end select
+    call VecSetSizes(vec_tmp, nlocal_out, PETSC_DECIDE, ierr)
+    call VecSetFromOptions(vec_tmp, ierr)
 
-      ! Copy local data into temporary vector, inserting values at natural index locations
-      call get_vector_data(v, vec_data)
-      call VecSetValues(vec_tmp, local_num_cells, topo%natural_indices(1:local_num_cells) - 1, &
-                        vec_data(1:local_num_cells), INSERT_VALUES, ierr)
-      call VecAssemblyBegin(vec_tmp, ierr)
-      call VecAssemblyEnd(vec_tmp, ierr)
-      call restore_vector_data(v, vec_data)
+    call VecSetValues(vec_tmp, nlocal_in, idx_to(1:nlocal_in) - 1, data_from, INSERT_VALUES, ierr)
+    call VecAssemblyBegin(vec_tmp, ierr)
+    call VecAssemblyEnd(vec_tmp, ierr)
 
-      ! Extract natural-indexed data into data array and return
-      call VecGetArrayReadF90(vec_tmp, vec_data, ierr)
-      if (allocated(data)) then ! Really shouldn't happen
-        deallocate (data)
-      end if
-      allocate (data(local_num_cells))
-      data(1:local_num_cells) = vec_data(1:local_num_cells)
-      call VecRestoreArrayReadF90(vec_tmp, vec_data, ierr)
+    call VecGetArrayReadF90(vec_tmp, data_tmp, ierr)
+    data_to(1:nlocal_out) = data_tmp(1:nlocal_out)
+    call VecRestoreArrayReadF90(vec_tmp, data_tmp, ierr)
 
-      ! Clean up
-      call VecDestroy(vec_tmp, ierr)
-
-    end associate
-
-  end subroutine get_natural_data_vec
+    call VecDestroy(vec_tmp, ierr)
+    
+  end subroutine reorder_data_vec
 
 !   module subroutine vec_view(vec_properties, vec)
 
---
+++ tests/utils/test_reorder_data.f90
@@ -0,0 +1,67 @@
+!!!v Test program for data reordering.
+
+program test_reorder_data
+
+  use testing_lib
+  
+  use utils, only: reorder_data
+  
+  implicit none
+
+  integer, parameter :: nglobal = 10
+  integer :: nlocal, istart
+  integer :: proc_id
+  
+  integer, dimension(:), allocatable :: to ! Destination address
+  real(ccs_real), dimension(:), allocatable :: src_data ! Data I hold
+  real(ccs_real), dimension(:), allocatable :: data ! Data I recieve
+
+  logical :: passing
+
+  integer :: i
+  
+  call init()
+
+  if(par_env%num_procs /= 4) then
+    call stop_test("This test only runs on 4 processors")
+  end if
+  
+  proc_id = par_env%proc_id
+  if(proc_id == 0) then
+    nlocal = 2
+    istart = 1
+    to = [ 7, 1 ]
+  else if (proc_id == 1) then
+    nlocal = 3
+    istart = 3
+    to = [ 2, 5, 8 ]
+  else if (proc_id == 2) then
+    nlocal = 3
+    istart = 6
+    to = [ 3, 10 ]
+  else ! Proc ID 3
+    nlocal = 2
+    istart = 9
+    to = [ 4, 6, 9 ]
+  end if
+  allocate(data(nlocal))
+  data(:) = 0
+  src_data = real(to)
+
+  call reorder_data(par_env, src_data, to, data)
+  
+  passing = .true.
+  do i = 1, nlocal
+    if (data(i) /= (istart + (i - 1))) then
+      passing = .false.
+    end if
+  end do
+
+  if (.not. passing) then
+    print *, proc_id, data
+    call stop_test("Test failed")
+  end if
+  
+  call fin()
+  
+end program test_reorder_data
