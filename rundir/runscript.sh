3d332cb962eb21ed1eea4a75280138970a39fc47
sb15895 <s2032563@ed.ac.uk>
2024-05-06 16:57:37 +0100

    runscript updates

--- rundir/runscript.sh
+++ rundir/runscript.sh
@@ -5,7 +5,6 @@ set -e
 submitSlurm() {
   export DARSHAN=${DARSHAN}
   export MESH=${MESH} 
-  export RUNDIR=$(pwd)/OUTPUT/LDC/${MESH}/${NUM_NODES}_${PPN} 
   if [[ $WAITFLAG  == 1 ]]
   then
     WAIT_CMD="--wait"
@@ -16,48 +15,44 @@ submitSlurm() {
   then 
   sbatch --export=ALL,DARSHAN=${DARSHAN},MESH=${MESH},RUNDIR=${RUNDIR} \
     --nodes=${NUM_NODES} --ntasks-per-node=${PPN} --time=${TIME} \
-    --array=${ARRAY} ${WAIT_CMD} \
+    --array=${ARRAY} --qos=${QOS} ${WAIT_CMD} \
     archer2.slurm 
   fi 
 }  
 
 weakScaling() {
-  echo "Entered weak scaling" 
   echo "Node start ${NODE_START} to Node end ${NODE_END} PPN ${PPN} time ${TIME}"
   echo "DARSHAN flag ${DARSHAN} wait flag ${WAITFLAG} job array ${ARRAY} dry run ${DRY_RUN} " 
   for i in $(seq ${NODE_START} ${NODE_END})
   do 
-    NUM_NODES=$((2**${i}))
+    NUM_NODES=$(( ${i} * 20 )) 
     submitSlurm 
   done
 } 
 
 # Default arguments 
 export DARSHAN=0
-export MESH=4096
-export NUM_NODES=1
+export MESH=$(( 400**3 ))  
 export PPN=128
-export TIME="00:20:00"
+export TIME="04:00:00"
 export ARRAY="0"
 export WAITFLAG=0 
 export DRY_RUN=0
+export CCS_CASE="TaylorGreenVortex"
 # Command line arguments 
 if  [[ $1 == 'test' ]]
 then 
-
+  export NUM_NODES=1
+  export RUNDIR=$(pwd)/test
   submitSlurm
-
 elif  [[ $1 == 'darshan' ]]
 then 
-
   export DARSHAN=1
   export NODE_START=1
   export NODE_END=4
-  export WAITFLAG=1
+  export WAITFLAG=0
+  export QOS="lowpriority"
   weakScaling
-
 else 
-
   echo 'invalid argument'
-
 fi 
